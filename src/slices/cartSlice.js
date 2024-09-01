c43e41c972d758a4057adf741850bdca3455e545
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-01 19:59:56 +1200

    copmlete cartSlice.js

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -37,7 +37,53 @@ const cartSlice = createSlice({
             }
 
             localStorage.setItem("cartItems", JSON.stringify(state.entities));
-        }
+        },
+        getTotals(state, action) {
+            let { total, qty } = Object.values(state.entities).reduce(
+                (cartTotal, cartItem) => {
+                    const { price, cartQty } = cartItem;
+                    const itemTotal = price * cartQty;
+
+                    cartTotal.total += itemTotal;
+                    cartTotal.qty += cartQty;
+
+                    return cartTotal;
+                },
+                {
+                    total: 0,
+                    qty: 0,
+                }
+            );
+            state.cartTotalQty = qty;
+            state.cartTotalAmount = total;
+        },
+        decreaseCart(state, action) {
+            const product = state.entities[action.payload.id];
+
+            if (product.cartQty > 1) {
+                product.cartQty -= 1;
+
+                toast.info("تعداد کاهش یافت", {
+                    position: "bottom-left",
+                });
+            } else if (product.cartQty === 1) {
+                cartAdapter.removeOne(state, action.payload.id);
+
+                toast.error("محصول از سبد خرید حذف شد", {
+                    position: "bottom-left",
+                });
+            }
+
+            localStorage.setItem("cartItems", JSON.stringify(state.entities));
+        },
+        removeFromCart(state, action) {
+            cartAdapter.removeOne(state, action.payload.id);
+
+            toast.error("محصول از سبد خرید حذف شد", {
+                position: "bottom-left",
+            });
+            localStorage.setItem("cartItems", JSON.stringify(state.entities));
+        },
     },
 });
 
@@ -46,7 +92,9 @@ export const { selectAll } = cartAdapter.getSelectors((state) => state.cart);
 export const {
     populateCart,
     addToCart,
-
+    decreaseCart,
+    removeFromCart,
+    getTotals,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
