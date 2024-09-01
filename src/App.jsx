267b6974113394ff25b934719db122670237d9e2
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-01 16:26:32 +1200

    add Header.jsx in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Helmet } from "react-helmet";
+
+import MainLayout from "./components/layouts/MainLayout";
+import Header from "./components/Header";
+import { useSelector } from "react-redux";
+import PaginateItems from "./components/common/PaginateItems";
+import { useGetProductsQuery } from "./slices/productApi";
+
+const App = () => {
+    const {
+        data: products = [],
+        isLoading,
+        isSuccess,
+        isError,
+    } = useGetProductsQuery();
+
+    return (
+        <MainLayout>
+            <Helmet>
+                <title>فروشگاه استیکر برنامه نویسی</title>
+            </Helmet>
+
+            <div className="mx-auto max-w-6xl">
+                <Header />
+            </div>
+        </MainLayout>
+    );
+};
+
+export default App;
