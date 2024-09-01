7424420cd0bdd414fc385320695ee9bb2fa9e4f8
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-01 16:25:40 +1200

    create component of Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div>
+            <h1
+                className="leading-relaxed font-primary font-extrabold text-4xl text-center
+                text-palette-primary mt-4 py-2 sm:py-4"
+            >
+                خرید بهترین استیکرهای برنامه نویسی
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
