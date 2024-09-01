658427ed21f2305541889f8783de50ab1af6ced3
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-01 19:52:05 +1200

    write extraReducer for response of stickers

--- src/slices/productSlice.js
+++ src/slices/productSlice.js
@@ -9,10 +9,31 @@ const initialState = {
 export const fetchProducts = createAsyncThunk(
     "products/fetchProducts",
     async () => {
-
+        try {
+            const response = await axios.get("http://localhost:9000/stickers");
+            return response.data;
+        } catch (err) {
+            console.log(err);
+        }
     }
 );
 
-
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [fetchProducts.pending]: (state, action) => {
+            state.status = "loading";
+        },
+        [fetchProducts.fulfilled]: (state, action) => {
+            state.items = action.payload;
+            state.status = "completed";
+        },
+        [fetchProducts.rejected]: (state, action) => {
+            state.status = "failed";
+        },
+    },
+});
 
 export default productsSlice.reducer;
