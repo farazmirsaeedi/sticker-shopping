93359dcb421651f4117249819046f4ba05b0a712
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-09-01 21:01:03 +1200

    change badge of cart icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import CustomNumeralNumericFormat from "./Price";
 import { selectAll } from "../slices/cartSlice";
 
 const Navbar = () => {
@@ -17,7 +18,20 @@ const Navbar = () => {
           </h1>
         </Link>
         <div>
-
+          <Link to="/cart" className="relative">
+            <i
+              className="fa fa-cart-arrow-down text-palette-primary m-auto"
+              style={{ fontSize: "30px" }}
+            ></i>
+            {cart.length === 0 ? null : (
+              <div className="absolute top-0 right-0 text-xs bg-yellow-300 text-gray-900 font-semibold rounded-full py-1 px-2 transform translate-x-4 -translate-y-4">
+                <CustomNumeralNumericFormat
+                  value={cart.length}
+                  thousandSeparator=","
+                />
+              </div>
+            )}
+          </Link>
         </div>
       </div>
     </header>
